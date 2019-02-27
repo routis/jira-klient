1c7122a5be42c5cf8fc98f34c0ea65af36521ed8
babis <haralampos.routis@intrasoft-intl.com>
2019-02-27 16:54:06 +0200

    Updated PromiseSupport

--- src/main/kotlin/routis/jira/klient/promiseSupport.kt
+++ src/main/kotlin/routis/jira/klient/promiseSupport.kt
@@ -24,44 +24,33 @@ interface PromiseSupport<F> {
      */
     fun <A> asKind(p: Promise<A>): Kind<F, A>
 
-    fun <C, A> withClient(getter: (Ctx) -> C, f: C.() -> Promise<A>): JiraReaderT<F, A> =
-        JiraReaderT { client ->
-            with(getter(client)) { f().pipe(::asKind) }
+    fun <C, A> withClient(get: (Ctx) -> C, doWith: C.() -> Promise<A>): JiraReaderT<F, A> =
+        JiraReaderT {
+            get(it).doWith().pipe(::asKind)
         }
 
-    fun <C, A> withClientLookup(getter: (Ctx) -> C, f: C.() -> Promise<A>): JiraReaderT<F, Option<A>> =
-        JiraReaderT { client ->
-            with(getter(client)) {
-                f().recover404WithNone().pipe(::asKind)
-            }
+    fun <C, A> withClientLookup(get: (Ctx) -> C, doWith: C.() -> Promise<A>): JiraReaderT<F, Option<A>> =
+        JiraReaderT {
+            get(it).doWith().recover404WithNone().pipe(::asKind)
         }
 }
 
-private fun <A> Promise<A>.recover404WithNone(): Promise<Option<A>> =
-    foldToEither().flatMap { either ->
+private fun <A> Promise<A>.recover404WithNone(): Promise<Option<A>> {
+
+    fun Throwable.isHttpError(httpErrorCode: Int) =
+        this is RestClientException && statusCode?.or(200) == httpErrorCode
+
+    fun foldToEither(): Promise<Either<Throwable, A>> =
+        fold({ it.left() }, { it.right() })
+
+    return foldToEither().flatMap { either ->
         either.fold(
             ifRight = { Promises.promise(it.some()) },
             ifLeft = {
-                when (it) {
-                    is EntityNotFound -> Promises.promise(none())
-                    is OtherError -> Promises.rejected(it.t)
-                }
+                if (it.isHttpError(404)) Promises.promise(none())
+                else Promises.rejected(it)
             })
     }
-
-private fun <A> Promise<A>.foldToEither(): Promise<Either<JiraOpsError, A>> =
-    fold({ of(it).left() }, { it.right() })
-
-
-private sealed class JiraOpsError
-private object EntityNotFound : JiraOpsError()
-private data class OtherError(val t: Throwable) : JiraOpsError()
-
-private fun of(t: Throwable): JiraOpsError {
-
-    fun isHttpError(httpErrorCode: Int) =
-        t is RestClientException && t.statusCode?.or(200) == httpErrorCode
-
-    return if (isHttpError(404)) EntityNotFound else OtherError(t)
 }
+
 
