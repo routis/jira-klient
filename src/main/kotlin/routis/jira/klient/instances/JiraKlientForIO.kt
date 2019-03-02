2fb8690b361e23c3696608684157aa521b9c3fd2
babis <haralampos.routis@intrasoft-intl.com>
2019-03-02 13:51:39 +0200

    Implemented JiraKlientForIO

--- src/main/kotlin/routis/jira/klient/instances/JiraKlientForIO.kt
+++ src/main/kotlin/routis/jira/klient/instances/JiraKlientForIO.kt
@@ -1,17 +1,13 @@
 package routis.jira.klient.instances
 
 import arrow.Kind
-import arrow.core.left
-import arrow.core.right
+import arrow.core.Either
 import arrow.effects.ForIO
 import arrow.effects.IO
 import arrow.effects.instances.io.async.async
 import arrow.effects.typeclasses.Async
-import arrow.syntax.function.pipe
 import io.atlassian.util.concurrent.Promise
-import io.atlassian.util.concurrent.Promises
 import routis.jira.klient.JiraKlient
-import java.util.concurrent.CompletableFuture
 
 @Suppress("unused")
 object JiraKlientForIO : JiraKlient<ForIO> {
@@ -20,20 +16,12 @@ object JiraKlientForIO : JiraKlient<ForIO> {
         get() = IO.async()
 
     override fun <A> asKind(p: Promise<A>): Kind<ForIO, A> =
-        Promises.toCompletableFuture(p).log() pipe { toIO2(it) }
-
-
-    private fun <A> toIO(f: CompletableFuture<A>): IO<A> =
-        IO.async { c, cb ->
-
-            f.handleAsync { success, error ->
-                error?.left() ?: success!!.right() pipe {cb(it)}
-            }
+        IO.async { _, cb ->
+            p.then(object : Promise.TryConsumer<A> {
+                override fun accept(t: A): Unit = cb(Either.right(t))
+                override fun fail(t: Throwable) = cb(Either.left(t))
+            })
         }
 
-    private fun <A> toIO2(f: CompletableFuture<A>): IO<A> =
-        IO.defer { IO.just(f.get()) }
 
-    private fun <A> CompletableFuture<A>.log(): CompletableFuture<A> =
-            this.thenApplyAsync { it.also { a->println("---->$a") } }
 }
