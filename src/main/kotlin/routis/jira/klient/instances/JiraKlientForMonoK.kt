4b50922f09fd2081ee65d47a9aff641ead608c59
babis <haralampos.routis@intrasoft-intl.com>
2019-03-12 14:33:30 +0200

    Directly created Mono from Promise bypassing the eagerly calculated CompletableFuture

--- src/main/kotlin/routis/jira/klient/instances/JiraKlientForMonoK.kt
+++ src/main/kotlin/routis/jira/klient/instances/JiraKlientForMonoK.kt
@@ -7,7 +7,6 @@ import arrow.effects.k
 import arrow.effects.monok.async.async
 import arrow.effects.typeclasses.Async
 import io.atlassian.util.concurrent.Promise
-import io.atlassian.util.concurrent.Promises
 import reactor.core.publisher.Mono
 import routis.jira.klient.JiraKlient
 
@@ -21,5 +20,12 @@ object JiraKlientForMonoK : JiraKlient<ForMonoK> {
         get() = MonoK.async()
 
     override fun <A> asKind(p: Promise<A>): Kind<ForMonoK, A> =
-        Promises.toCompletableFuture(p).let { Mono.fromFuture(it).k() }
+        Mono.create<A> { sink ->
+            p.then(object : Promise.TryConsumer<A> {
+                override fun accept(t: A): Unit = sink.success(t)
+                override fun fail(t: Throwable): Unit = sink.error(t)
+            })
+        }.k()
+
 }
+
